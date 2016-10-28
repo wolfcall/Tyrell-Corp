0052f77fa130b7a27a30ba1e25089dd368931604
Chris Paslawski <chris@paslawski.me>
2016-10-27 19:12:33 -0500

    Install Laravel framework

--- install.sh
+++ install.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Install script"
+###
+# Deployment
+###
 
+# download or update composer
+if [ ! -f composer.phar ]; then
+    curl -sS https://getcomposer.org/installer | php
+else
+    php composer.phar self-update
+fi
+
+# bring the application down for maintenance
+touch storage/framework/down
+echo "Application is now down."
+
+# remove compiled cache files
+rm -f bootstrap/cache/*
+
+# install packages
+php composer.phar install --no-dev --no-interaction --prefer-dist
+
+# run any outstanding migrations
+php artisan migrate --force
+
+# optimize autoloader and compile common classes
+php artisan optimize --force
+
+# clear application cache
+php artisan cache:clear
+
+# and bring the application back up
+rm -f storage/framework/down
+echo "Application is now live."
