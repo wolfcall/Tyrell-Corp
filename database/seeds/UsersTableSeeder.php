3b039116c0da3538e7053fc98778082a46101f5c
PopMyStack <julian.ippolito@hotmail.com>
2017-03-12 15:28:02 -0400

    Cleaning up the seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
-{
+class UsersTableSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         for ($i = 1; $i <= 5; $i++) {
-            
+
             DB::table('users')->insert([
                 'id' => $i,
                 'name' => 'Test User ' . $i,
@@ -20,4 +19,5 @@ class UsersTableSeeder extends Seeder
             ]);
         }
     }
+
 }
