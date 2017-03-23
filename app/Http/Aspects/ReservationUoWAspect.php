ba77b23684f4f6da2d6ac43dc32d7da1dc135e38
wolfcall <gmathieu6@gmail.com>
2017-03-23 18:28:16 -0400

    Cleanup 2

--- app/Http/Aspects/ReservationUoWAspect.php
+++ app/Http/Aspects/ReservationUoWAspect.php
@@ -35,8 +35,7 @@ class ReservationUoWAspect implements Aspect {
      * @Around("execution(public App\Data\UoWs\ReservationUoW->registerNew(*))")
      */
     public function aroundRegisterNewExecution(MethodInvocation $invocation) {
-        var_dump("around New");
-        
+
         $passing = $invocation->getArguments();
 
         $this->newList[] = $passing[0];
@@ -49,8 +48,7 @@ class ReservationUoWAspect implements Aspect {
      * @Around("execution(public App\Data\UoWs\ReservationUoW->registerDirty(*))")
      */
     public function aroundRegisterDirtyExecution(MethodInvocation $invocation) {
-        var_dump("around Dirty");
-        
+
         $passing = $invocation->getArguments();
 
         $this->changedList[] = $passing[0];
@@ -63,8 +61,7 @@ class ReservationUoWAspect implements Aspect {
      * @Around("execution(public App\Data\UoWs\ReservationUoW->registerDeleted(*))")
      */
     public function aroundRegisterDeletedExecution(MethodInvocation $invocation) {
-        var_dump("around Delete");
-        
+
         $passing = $invocation->getArguments();
 
         $this->deletedList[] = $passing[0];
@@ -77,8 +74,7 @@ class ReservationUoWAspect implements Aspect {
      * @Around("execution(public App\Data\Mappers\ReservationMapper->done(*))")
      */
     public function aroundDoneExecution(MethodInvocation $invocation) {
-        var_dump("around Done");
-        
+
         $this->commit();
     }
 
@@ -87,26 +83,18 @@ class ReservationUoWAspect implements Aspect {
      * Commit all the changes by passing the lists to the Reservation mapper
      * 
      */
-    
     public function commit() {
-        var_dump("committing");
-        
+
         $this->mapper = ReservationMapper::getInstance();
 
         $this->mapper->addMany($this->newList);
         $this->mapper->updateMany($this->changedList);
         $this->mapper->deleteMany($this->deletedList);
 
-        var_dump($this->newList);
-        var_dump($this->changedList);
-        var_dump($this->deletedList);
-
         // empty the lists after the commit
         $this->newList = [];
         $this->changedList = [];
         $this->deletedList = [];
-        
-        die();
     }
 
 }
